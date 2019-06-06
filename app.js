dd34035d1af8d7dfb8727df672231ece1bdface7
willissa2121 <you@example.com>
2019-06-05 20:18:10 -0700

    working

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ let winVar = 0;
 let loseVar = 0;
 let timeCount = 15;
 var timerBaby;
+let timerReset = true
+let musicCounter = 0
+let audio = document.getElementById("theme-song")
 
 //Array of objects containing all of the displayed content
 
@@ -51,60 +54,57 @@ let questionBanks = [
   }
 ]
 
+
+
 //timerfunction used to declare and call the timout function
-let startTimer = () => {
-  var timerBaby = setInterval(myTimer, 1000);
+let startTimer = (timerReset) => {
+  console.log(timeCount)
+  if (timerReset === 0) {
+    timerBaby = setInterval(function () {
+      if (timeCount > 0) {
+
+        $("#append-timer").empty();
+        $("#append-timer").append(timeCount);
+        timeCount--
+
+
+      }
+      else if (timeCount === 0) {
+        $("#append-timer").empty();
+        clearInterval(timerBaby)
+        displayTimerLose()
+      }
+    }
+      , 1000);
+  }
+  else if (timerReset === 1) {
+    clearInterval(timerBaby)
+  }
 }
 
 
-let myTimer = () => {
-  $("#append-timer").empty();
-  $("#append-timer").append(timeCount);
-  timeCount --
-}
-
-function myStopFunction() {
-  timeCount = 15
-  clearInterval(timerBaby);
-}
-
-
-// let genQuestion = () => {
-//   for(var i = 0; i < questionBanks.length; i++){
-//     let chosenQuestion = questionBanks[i]
-//     for (var j = 0; j < 4; j++){
-//       let answerGen = chosenQuestion.answers[j]
-//       console.log(answerGen)
-//     }
-//   }
-
-// }
-// genQuestion()
 
 //populates the quiz field with the next object of questions and potential answers 
 
 let genQuestion = (i) => {
-  // document.getElementById("question").innerHTML= (questionBanks[0].question)
   if (counter < questionBanks.length) {
-    myStopFunction()
     $("#question").html(questionBanks[i].question)
     $("#answer1").html(questionBanks[i].answers[0])
     $("#answer2").html(questionBanks[i].answers[1])
     $("#answer3").html(questionBanks[i].answers[2])
     $("#answer4").html(questionBanks[i].answers[3])
-    
-
   }
-
 }
 
-// genQuestion(0);
+
 
 //button that reveals and hides approproate header content, calls genquestion for its origin
 
 $("#start-button").click(function () {
+  
+  audio.play()
+  startTimer(0)
   genQuestion(0)
-  startTimer();
   $("#start-button").css("display", "none")
   $("#hide-me").css("display", "none")
   $(".clickable").css("display", "block")
@@ -114,6 +114,8 @@ $("#start-button").click(function () {
 //Function used to empty all fields after questions is selected, then will repopulate by calling genQuestion at its counted indices (counter adds up in checked answers function)
 
 let newQuestions = () => {
+  timeCount = 15;
+  startTimer(0)
 
   $("#append-header").empty()
   $(".clickable").css("display", "block")
@@ -134,47 +136,63 @@ $("#answer1").click(function () {
   $("#append-header").empty()
   let answer = ($("#answer1").text())
   checkAnswer(answer)
+  timeCount = 15
+  startTimer(1)
+
 
 })
 $("#answer2").click(function () {
   $("#append-header").empty()
   let answer = ($("#answer2").text())
   checkAnswer(answer)
+  timeCount = 15
+  startTimer(1)
 })
 $("#answer3").click(function () {
   $("#append-header").empty()
   let answer = ($("#answer3").text())
   checkAnswer(answer)
+  timeCount = 15
+  startTimer(1)
 
 })
 $("#answer4").click(function () {
   $("#append-header").empty()
   let answer = ($("#answer4").text())
   checkAnswer(answer)
+  timeCount = 15
+  startTimer(1)
 })
 
 
 // string comparison of choices vs Correct Answer
 let checkAnswer = (x) => {
   if (x === questionBanks[counter].rightAnswer) {
-    myStopFunction()
 
     counter++
     // console.log("youre the winner")
     displayWin();
     $("#append-timer").css("display", "none")
-
-    setTimeout(newQuestions, 3000)
+    if (counter < questionBanks.length) {
+      setTimeout(newQuestions, 3000)
+    }
+    else{
+      setTimeout(finalScreen,3000)
+    }
   }
   else {
-    myStopFunction()
 
     counter++
     // console.log("youre a loser")
     $("#append-timer").css("display", "none")
 
     displayLose()
-    setTimeout(newQuestions, 3000)
+    if (counter < questionBanks.length) {
+      setTimeout(newQuestions, 3000)
+    }
+    else{
+      setTimeout(finalScreen,3000)
+    }
   }
 
 }
@@ -191,6 +209,9 @@ let displayWin = () => {
   $("#question").empty()
 
   $("#question").append(questionBanks[counter - 1].rightAnswer)
+  let swish = document.getElementById("swish")
+  swish.play()
+
 }
 
 // same as winner header but a loser choice if strimng comaprison does not match
@@ -205,61 +226,55 @@ let displayLose = () => {
   $("#append-header").append(loserHeader)
   $("#question").empty()
   $("#question").append(`The Correct Answer is ${questionBanks[counter - 1].rightAnswer}`)
+  let sadness = document.getElementById("sadness")
+  sadness.play()
+  // console.log(counter)
+
+}
+//loss screen based on losing due to timer timeOut
+let displayTimerLose = () => {
+  loseVar++;
+  counter++
+  $("#wrong-guess").text(loseVar)
+  $(".clickable").css("display", "none")
+  let loserHeader = $("<h1>")
+  loserHeader.text("Out of Time")
+  loserHeader.css("color", "red")
+  $("#append-header").append(loserHeader)
+  $("#question").empty()
+  console.log(counter)
+  console.log(questionBanks[counter - 1].rightAnswer)
+  $("#question").append(`The Correct Answer is ${questionBanks[counter - 1].rightAnswer}`)
+  setTimeout(newQuestions, 3000)
+  sadness.play()
+
 }
 
+let finalScreen = () => {
+  
+}
+
+$("#music-button").click(function(){
+  if(musicCounter === 0){
+    audio.pause()
+    $("#music-button").empty()
+    $("#music-button").text("Play Music")
+    musicCounter++
+  }
+  else{
+    audio.play()
+    $("#music-button").empty()
+    $("#music-button").text("Pause Music")
+    musicCounter--
+
+  }
+})
 
 
 
 
 
-// var files = [
-//   "pavans_first_birthday.mov",
-//   "owens_asleep_at_the_computer.jpg",
-//   "michael_fights_a_polar_bear.mp4",
-//   "nate_road_rage.avi",
-//   "ruby_skydiving.jpeg",
-//   "ken_getting_his_black_belt.png",
-//   "dan_winning_underground_street_race.mov",
-//   "its_hard_to_come_up_with_file_names.gif",
-//   "seriously_this_is_taking_too_long.mpg",
-//   "i_wonder_how_many_of_these_i_should_have.png",
-//   "probably_a_few_more.avi",
-//   "nutmeg_is_clawing_my_sneakers_again.mp4",
-//   "cat_i_will_destroy_you.gif",
-//   "i_wish_we_had_a_dog.jpeg",
-//   "stop_looking_at_me_like_that_nutmeg.mpeg",
-//   "aww_i_cant_hate_you.png",
-//   "omg_my_sneakers.avi",
-//   "cat_you_are_the_worst.mp4"
-// ]
 
-// let fileSorter = (files)=>{
-//   let movieArray = []
-//   let musicArray = []
-//   for (var i = 0; i <files.length;i++ ){
-//     let currentFile = files[i]
 
-//     if(currentFile[currentFile.length-4]!== "."){
-//       if (currentFile[currentFile.length-4]=== "m"){
-//         movieArray.push(currentFile)
-//       }
-//       else{
-//         musicArray.push(currentFile)
-//       }
-//     }
-
-//     else if (currentFile[currentFile.length-3]==="m" ||currentFile[currentFile-3]==="a"){
-//       movieArray.push(files[i])
-//       console.log(movieArray)
-//     }
-//     else if (currentFile[currentFile.length-3]==="j" ||currentFile[currentFile-3]==="p" || currentFile[currentFile.length-3]==="p"){
-//       musicArray.push(files[i])
-//       console.log(musicArray)
-//     }
-
-//   }
-// }
-
-// fileSorter(files)
 
 
